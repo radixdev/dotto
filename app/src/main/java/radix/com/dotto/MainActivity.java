a1115fd0475d026bac46db01173b82931fb894d3
Julian Contreras <julian@appboy.com>
2017-04-19 19:45:06 -0400

    updates to the drawing

--- app/src/main/java/radix/com/dotto/MainActivity.java
+++ app/src/main/java/radix/com/dotto/MainActivity.java
@@ -1,26 +1,84 @@
 package radix.com.dotto;
 
 import android.os.Bundle;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 
-public class MainActivity extends AppCompatActivity {
+import radix.com.dotto.models.WorldMap;
+import radix.com.dotto.views.PixelGridSurfaceView;
+
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
+  private static final String TAG = MainActivity.class.toString();
+
+  private PixelGridSurfaceView gameView;
+  private WorldMap worldMap;
+  private GestureDetectorCompat mDetector;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    
-    setContentView();
-//    setContentView(R.layout.activity_main);
-//    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//    setSupportActionBar(toolbar);
-//
-//    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//    fab.setOnClickListener(new View.OnClickListener() {
-//      @Override
-//      public void onClick(View view) {
-//        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//            .setAction("Action", null).show();
-//      }
-//    });
+
+    worldMap = new WorldMap();
+    gameView = new PixelGridSurfaceView(this, worldMap);
+    setContentView(gameView);
+
+    mDetector = new GestureDetectorCompat(this, this);
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    gameView.setPlaying(false);
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    gameView.setPlaying(true);
+  }
+
+  @Override
+  public boolean onTouchEvent(MotionEvent event) {
+    this.mDetector.onTouchEvent(event);
+    return super.onTouchEvent(event);
+  }
+
+  @Override
+  public boolean onDown(MotionEvent motionEvent) {
+    return false;
+  }
+
+  @Override
+  public void onShowPress(MotionEvent motionEvent) {
+
+  }
+
+  @Override
+  public boolean onSingleTapUp(MotionEvent motionEvent) {
+    return false;
+  }
+
+  @Override
+  public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+    int ty = (int) e2.getY();
+    int tx = (int) e2.getX();
+
+    gameView.tx = tx;
+    gameView.ty = ty;
+
+//    Log.v(TAG, "onScroll: " + e1.toString()+e2.toString());
+    return true;
+  }
+
+  @Override
+  public void onLongPress(MotionEvent motionEvent) {
+
+  }
+
+  @Override
+  public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+    return false;
   }
 }
--- app/src/main/java/radix/com/dotto/views/PixelGridSurfaceView.java
+++ app/src/main/java/radix/com/dotto/views/PixelGridSurfaceView.java
@@ -1,42 +1,113 @@
 package radix.com.dotto.views;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import java.util.Random;
+
 import radix.com.dotto.models.WorldMap;
 
-public class PixelGridSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+public class PixelGridSurfaceView extends SurfaceView implements Runnable, SurfaceHolder.Callback {
+  private static final String TAG = PixelGridSurfaceView.class.toString();
 
-  private final WorldMap worldMap;
-  private final SurfaceHolder holder;
+  private final WorldMap mWorldMap;
+
+  // The
+  private final SurfaceHolder mSurfaceHolder;
+  private volatile boolean mIsGamePlaying;
+  private Thread mGameThread = null;
+
+  // The drawing properties
+  private Bitmap mCanvasBitmap = null;
+  private Canvas mBackingCanvas = null;
+  private Matrix mTransformMatrix;
+
+  private Random random;
 
   public PixelGridSurfaceView(Context context, WorldMap map) {
     super(context);
-    worldMap = map;
+    mWorldMap = map;
 
-    holder = getHolder();
-    holder.addCallback(this);
+    mSurfaceHolder = getHolder();
+    mSurfaceHolder.addCallback(this);
+    random = new Random();
   }
+
+  @Override
+  public void run() {
+    while (mIsGamePlaying) {
+      draw();
+
+      // Sleep a bit maybe
+      try {
+        Thread.sleep(16);
+      } catch (InterruptedException e) {
+        Log.e(TAG, "Exception while sleeping in game loop", e);
+      }
+    }
+  }
+
+  private void draw() {
+    if (mSurfaceHolder.getSurface().isValid()) {
+      Canvas canvas = mSurfaceHolder.lockCanvas();
+      this.draw(canvas);
+      mSurfaceHolder.unlockCanvasAndPost(canvas);
+    }
+  }
+
+  public volatile int tx=0, ty=0;
 
   @Override
   public void draw(Canvas canvas) {
     super.draw(canvas);
+
+    final Paint pixelPaint = new Paint();
+    pixelPaint.setColor(Color.WHITE);
+    for (int i = 0; i < 32; i++) {
+      mBackingCanvas.drawPoint(random.nextInt(1000), random.nextInt(1000), pixelPaint);
+    }
+
+    mTransformMatrix.setTranslate(tx, ty);
+    canvas.drawBitmap(mCanvasBitmap, mTransformMatrix, null);
+  }
+
+  public void setPlaying(boolean playing) {
+    Log.d(TAG, "Set playing to : " + playing);
+    this.mIsGamePlaying = playing;
+
+    if (playing) {
+      mGameThread = new Thread(this);
+      mGameThread.start();
+    } else {
+      try {
+        mGameThread.join();
+      } catch (InterruptedException e) {
+        Log.e(TAG, "Set playing end thread exception", e);
+      }
+    }
   }
 
   @Override
   public void surfaceCreated(SurfaceHolder surfaceHolder) {
+    int width = getWidth();
+    int height = getHeight();
+    mCanvasBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+    mBackingCanvas = new Canvas();
+    mBackingCanvas.setBitmap(mCanvasBitmap);
 
+    mTransformMatrix = new Matrix();
   }
 
   @Override
-  public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-
-  }
+  public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {}
 
   @Override
-  public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-
-  }
+  public void surfaceDestroyed(SurfaceHolder surfaceHolder) {}
 }
