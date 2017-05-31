fb30a7114fcb4f937ad059691456d42096403f1d
Julian Contreras <jrcontre@mit.edu>
2017-05-30 21:35:36 -0400

    fixed the immersive mode

--- app/src/main/java/radix/com/dotto/DottoActivity.java
+++ app/src/main/java/radix/com/dotto/DottoActivity.java
@@ -58,13 +58,8 @@ public class DottoActivity extends AppCompatActivity {
         WindowManager.LayoutParams.FLAG_FULLSCREEN,
         WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-    View decorView = getWindow().getDecorView();
-    decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-        | View.SYSTEM_UI_FLAG_FULLSCREEN
-        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    // This is present here since without it, the detected screen width in the surfaceView is wrong.
+    grantImmersiveModeToWindow();
 
     setContentView(R.layout.activity_main);
     setupGameState();
@@ -105,6 +100,22 @@ public class DottoActivity extends AppCompatActivity {
       public void onUserWriteSucceeded() {
       }
     });
+  }
+
+  @Override
+  public void onWindowFocusChanged(boolean hasFocus) {
+    super.onWindowFocusChanged(hasFocus);
+    grantImmersiveModeToWindow();
+  }
+
+  private void grantImmersiveModeToWindow() {
+    View decorView = getWindow().getDecorView();
+    decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        | View.SYSTEM_UI_FLAG_FULLSCREEN
+        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
   }
 
   private void setupModelUpdates() {
